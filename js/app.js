b471c6669dc4bcde11ce0ac2ba260ad389d0bcc8
manu <manu.villar.vitoria@gmail.com>
2020-05-18 19:52:59 +0200

    interface's selectors

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let DB;
+
+//Selectores de la Interfaz
+const form = document.querySelector('#form'),
+      nombreMascota = document.querySelector('#mascota'),
+      nombreCliente = document.querySelector('#cliente'),
+      telefono = document.querySelector('#telefono'),
+      fecha = document.querySelector('#fecha'),
+      hora = document.querySelector('#hora'),
+      sintomas = document.querySelector('#sintomas'),
+      citas = document.querySelector('#citas'),
+      headingAdministra = document.querySelector('#administra');
