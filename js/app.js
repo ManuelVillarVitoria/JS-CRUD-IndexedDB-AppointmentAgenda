35bfc0f4c0ee3035f3e2d3f7a10e020a4dfd1133
manu <manu.villar.vitoria@gmail.com>
2020-05-18 20:07:33 +0200

    creating data base

--- js/app.js
+++ js/app.js
@@ -10,3 +10,26 @@ const form = document.querySelector('#form'),
       sintomas = document.querySelector('#sintomas'),
       citas = document.querySelector('#citas'),
       headingAdministra = document.querySelector('#administra');
+
+
+//Esperar por le DOM Ready
+document.addEventListener('DOMContentLoaded',() => {
+    //Crear la base de datos
+    //Los parámetros son el nombre de la base de datos y el 
+    //número es la versión(siempre ha de ser un número entero)
+    let crearDB = window.indexedDB.open('citas', 1); 
+
+    //Si hay un error enviarlo a la consola
+    crearDB.onerror = function() {
+        console.log ('Hubo un error');
+    }
+    //Si todo está bien muestra en consola, y asignar la base de datos
+    crearDB.onsuccess = function() {
+        console.log ('Todo listo');
+
+        //Asignar a la base de Datos
+        DB = crearDB.result;
+        console.log(DB);
+    }
+    
+})
